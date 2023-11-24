e3c9327d3ca9cc71738f8d3d8e247e0c168a2bb7
Tomáš Šmerda <tomas.smerda@applifting.cz>
2023-11-24 17:34:20 +0100

    Initial commit

---
+++ MeteoritesNASA/MeteoritesNASAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteoritesNASAApp.swift
+//  MeteoritesNASA
+//
+//  Created by Tomáš Šmerda on 24.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MeteoritesNASAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
